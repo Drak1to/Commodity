dcf5603dfdf6069939a4d245ef592338dfbb8313
Drak1to <128966372+Drak1to@users.noreply.github.com>
2023-10-12 19:04:40 +0200

    Update ComparatorOfProducts.java

--- src/ComparatorOfProducts.java
+++ src/ComparatorOfProducts.java
@@ -1,15 +1,24 @@
-import java.util.Comparator;
-
 public class ComparatorOfProducts  implements Comparator<Products> {
     @Override
     public int compare(Products o1, Products o2) {
-        if(o1.getName().compareTo(o2.getName()) < 0){
+        if (o1.getName().compareTo(o2.getName()) < 0) {
             return -1;
-        } else if (o1.getName().compareTo(o2.getName()) > 0){
+        } else if (o1.getName().compareTo(o2.getName()) > 0) {
             return 1;
+        } else if (o1.getLength() > o2.getLength()) {
+            return 1;
+        } else if (o1.getLength() < o2.getLength()) {
+            return -1;
+        } else if (o1.getWidth() > o2.getWidth()) {
+            return 1;
+        } else if (o1.getWidth() < o2.getWidth()) {
+            return -1;
+        } else if (o1.getWeight() > o2.getWeight()) {
+            return 1;
+        } else if (o1.getWeight() < o2.getWeight()) {
+            return -1;
         } else {
             return 0;
         }
     }
-    }
-
+}
