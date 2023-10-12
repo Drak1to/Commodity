94d04534dd270b7e2f6ee14436fef0ae14d6f181
Drak1to <128966372+Drak1to@users.noreply.github.com>
2023-10-12 19:03:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,43 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 public class Main {
     public static void main(String[] args) {
-        
+        Products products = new Products("T-Shirt", "Clothes",50, 2,25);
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            menu();
             String choose = scanner.next();
             scanner.nextLine();
             switch (choose) {
                 case "1": {
-                    ;
+                   products.addProducts();
                     break;
                 }
+                case "2": {
+                    products.deleteProduct();
+                    break;
+                }
+                case "3" : {
+                    products.showProducts();
+                    break;
+                }
+                case "4" : {
+                    products.replaceProduct();
+                    break;
+                }
+
             }
         }
     }
-    private static void menu(){
+
+    private static void menu() {
         System.out.println("--------------------------------------------");
         System.out.println("Enter 1 to add product");
         System.out.println("Enter 2 to remove product");
         System.out.println("Enter 3 to display all products ");
-
+        System.out.println("Enter 4 to replace product");
     }
 }
